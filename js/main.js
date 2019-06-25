3b54f127d47c7a5750716ab2bab934260bc80a72
samidahlawi <o.t5@hotmail.com>
2019-06-25 17:26:02 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+// When the #new form is submitted, add the new item
+
+
+// Functions for add image
+function addImage(event){
+		// 	Prevent page reload
+
+		//	Get the text the user entered
+
+		//	Select the 'ul' by 'id' use Jquery and assgin the 'ul' element to variable called list
+
+		// 	Try to fetch the data from 'giphy' API, and set the method as 'get' by using axios
+		//	.then wants a function to run if the request is succesfu 
+		//	get the value of 'url' key in the object and assign it to variable called image
+	 	//	append a 'li' within img in the unorder list
+	    //  .catch wants a function to run if the request is fails
+		
+
+}
+	
+});
